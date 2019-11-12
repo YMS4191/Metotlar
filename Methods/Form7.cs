649c060ce89cc9b121e7e49ccc170639f3b99044
muratvuranok <mrtvrnk@gmail.com>
2019-11-12 10:53:36 +0300

    Intro

--- Methods/Form7.cs
+++ Methods/Form7.cs
@@ -16,5 +16,40 @@ namespace Methods
         {
             InitializeComponent();
         }
+
+
+
+        void Temizle(Control ctrl)
+        {
+            foreach (Control item in ctrl.Controls)
+            {
+                if (item is TextBox)
+                {
+                    item.Text = "";
+                }
+                else if (item is NumericUpDown)
+                {
+                    NumericUpDown nmr = (NumericUpDown)item;
+                    nmr.Value = nmr.Minimum;
+                }
+                else if (item is PictureBox)
+                {
+                    PictureBox pcb = (PictureBox)item;
+                    pcb.Image = null;
+                }
+                else if (item is GroupBox)
+                {
+                    Temizle(item);
+                }
+            }
+        }
+
+        private void btnOrnek1_Click(object sender, EventArgs e)
+        {
+            Temizle(this);
+            Temizle(groupBox1);
+            Temizle(groupBox2);
+            Temizle(groupBox3);
+        }
     }
 }
